ec04a457481cdd59976e5f360796a45f454a2aec
milesar <adam.miles@utah.edu>
2018-09-24 07:55:53 -0600

    refined navigation

--- app/src/main/java/cs6018/lifestyleapp/HomeActivity.java
+++ app/src/main/java/cs6018/lifestyleapp/HomeActivity.java
@@ -5,11 +5,13 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
-import android.widget.FrameLayout;
 
 public class HomeActivity extends AppCompatActivity {
 
-    private FrameLayout fragContainer;
+    GoalsFrag goalsFrag = new GoalsFrag();
+    StatsFrag statsFrag = new StatsFrag();
+    ToolsFrag toolsFrag = new ToolsFrag();
+    ProfileFrag profileFrag = new ProfileFrag();
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -18,7 +20,7 @@ public class HomeActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_stats:
-                    //mTextMessage.setText(R.string.title_stats);
+
                     return true;
                 case R.id.navigation_goals:
                     //mTextMessage.setText("Goals");
@@ -27,7 +29,10 @@ public class HomeActivity extends AppCompatActivity {
                     //mTextMessage.setText(R.string.title_profile);
                     return true;
                 case R.id.navigation_tools:
-                    //mTextMessage.setText(R.string.title_tools);
+                    //mTextMessage.setText(R.string.title_stats);
+                    // Add the fragment to the 'fragment_container' FrameLayout
+                    getSupportFragmentManager().beginTransaction()
+                            .add(R.id.main_fragment_container, toolsFrag).commit();
                     return true;
             }
             return false;
@@ -41,6 +46,24 @@ public class HomeActivity extends AppCompatActivity {
 
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        // Check that the activity is using the layout version with
+        // the fragment_container FrameLayout
+        if (findViewById(R.id.main_fragment_container) != null) {
+
+            // However, if we're being restored from a previous state,
+            // then we don't need to do anything and should return or else
+            // we could end up with overlapping fragments.
+            if (savedInstanceState != null) {
+                return;
+            }
+            // Create a new Fragment to be placed in the activity layout
+            toolsFrag.setRetainInstance(true);
+            profileFrag.setRetainInstance(true);
+            goalsFrag.setRetainInstance(true);
+            statsFrag.setRetainInstance(true);
+        }
+
     }
 
 }
