ea2f28af4421d6627ee458ac8dcf3dda93b5c2a6
milesar <adam.miles@utah.edu>
2018-09-25 12:43:25 -0600

    added user class and updated design report

--- app/src/main/java/cs6018/lifestyleapp/StatsFrag.java
+++ app/src/main/java/cs6018/lifestyleapp/StatsFrag.java
@@ -7,102 +7,59 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.TextView;
 
 /**
  * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link StatsFrag.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link StatsFrag#newInstance} factory method to
  * create an instance of this fragment.
  */
 public class StatsFrag extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private TextView mTitle;
 
-    private OnFragmentInteractionListener mListener;
 
     public StatsFrag() {
         // Required empty public constructor
     }
 
     /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment StatsFrag.
+     * unpack the user profile data.
+     * @param savedInstanceState
      */
-    // TODO: Rename and change types and number of parameters
-    public static StatsFrag newInstance(String param1, String param2) {
-        StatsFrag fragment = new StatsFrag();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
+    /**
+     * place the user profile data in the appropriate components.
+     * @param inflater
+     * @param container
+     * @param savedInstanceState
+     * @return
+     */
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_stats, container, false);
-    }
+        super.onCreate(savedInstanceState);
+        View view = inflater.inflate(R.layout.fragment_stats, container, false);
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
+        Bundle bundle = this.getArguments();
+
+        //Get the validated user names
+
+        return view;
     }
 
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
     }
 
     @Override
     public void onDetach() {
         super.onDetach();
-        mListener = null;
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
-    }
 }
--- app/src/main/java/cs6018/lifestyleapp/User.java
+++ app/src/main/java/cs6018/lifestyleapp/User.java
@@ -2,5 +2,114 @@ package cs6018.lifestyleapp;
 
 public class User {
 
+    // User data.
+    private String userName = "Ann12345";
+    private String sex = "Female";
+    private String city = "Swansea";
+    private String nation = "Wales";
+    private Integer height = 120;
+    private Integer weight = 145;
+
+    // User goals.
+    private Integer targetWeight = 130;
+    private Integer targetBMI = 22;
+
+    private Integer targetHikes = 3;
+    private Integer targetDailyCalories = 2000;
+    private enum Season {
+        LOOSE, GAIN, MAINTAIN
+    }
+
+    public Integer getTargetBMI() {
+        return targetBMI;
+    }
+
+    public void setTargetBMI(Integer targetBMI) {
+        this.targetBMI = targetBMI;
+    }
+
+    public Integer getTargetHikes() {
+        return targetHikes;
+    }
+
+    public void setTargetHikes(Integer targetHikes) {
+        this.targetHikes = targetHikes;
+    }
+
+    public Integer getTargetDailyCalories() {
+        return targetDailyCalories;
+    }
+
+    public void setTargetDailyCalories(Integer targetDailyCalories) {
+        this.targetDailyCalories = targetDailyCalories;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    private String age = "38";
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getNation() {
+        return nation;
+    }
+
+    public void setNation(String nation) {
+        this.nation = nation;
+    }
+
+    public Integer getHeight() {
+        return height;
+    }
+
+    public void setHeight(Integer height) {
+        this.height = height;
+    }
+
+    public Integer getWeight() {
+        return weight;
+    }
+
+    public void setWeight(Integer weight) {
+        this.weight = weight;
+    }
+
+    public Integer getTargetWeight() {
+        return targetWeight;
+    }
+
+    public void setTargetWeight(Integer targetWeight) {
+        this.targetWeight = targetWeight;
+    }
+
+
 
 }
