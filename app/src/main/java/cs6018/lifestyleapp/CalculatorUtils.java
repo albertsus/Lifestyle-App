bef65048341206de33d1377722514a7b73052058
albertsu <suchaofan19950207@gmail.com>
2018-09-28 01:32:05 -0600

    height unit to inches

--- app/src/main/java/cs6018/lifestyleapp/CalculatorUtils.java
+++ app/src/main/java/cs6018/lifestyleapp/CalculatorUtils.java
@@ -4,23 +4,17 @@ public class CalculatorUtils {
 
     public static int computeBMI(String weight, String height) {
         int lbs = Integer.parseInt(weight);
-        int inches = parse(height);
+        int inches = Integer.parseInt(height);
         return lbs * 703 / (inches * inches);
     }
 
     public static int computeBMR(String weight, String height, String sex, String age) {
         int lbs = Integer.parseInt(weight);
-        int inches = parse(height);
+        int inches = Integer.parseInt(height);
         int ageNum = Integer.parseInt(age);
         return sex.equals("Male") ?
                 ((66 + (6 * lbs) + (12 * inches) - (6 * ageNum))) :
                 ((655 + (4 * lbs) + (5 * inches) - (7 * ageNum)));
     }
 
-    private static int parse(String height) {
-        String[] w = height.split("\'");
-        String feet = w[0].trim();
-        String inches = w[1].replace("\"", "").trim();
-        return Integer.parseInt(feet) * 12 + Integer.parseInt(inches);
-    }
 }
