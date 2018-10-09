1569a73a48077f9edebe964720aa0349e2c6b50d
milesar <adam.miles@utah.edu>
2018-10-09 12:20:50 -0600

    updating weather functionality

--- app/src/main/java/cs6018/lifestyleapp/WeatherActivity.java
+++ app/src/main/java/cs6018/lifestyleapp/WeatherActivity.java
@@ -1,11 +1,20 @@
 package cs6018.lifestyleapp;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
 import android.support.annotation.Nullable;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 public class WeatherActivity extends AppCompatActivity {
 
@@ -20,11 +29,15 @@ public class WeatherActivity extends AppCompatActivity {
     private TextView mTvUVIndex;
 
     private WeatherViewModel mWeatherViewModel;
+    private FusedLocationProviderClient mFusedLocationClient;
+    private Location mLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
 
         mTvLocation = (TextView) findViewById(R.id.tv_location);
         mTvWeatherSummary = (TextView) findViewById(R.id.tv_weather_summary);
@@ -43,6 +56,19 @@ public class WeatherActivity extends AppCompatActivity {
         //Set the observer
         mWeatherViewModel.getData().observe(this, nameObserver);
 
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            mFusedLocationClient.getLastLocation()
+                    .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                        @Override
+                        public void onSuccess(Location location) {
+                            // Got last known location. In some rare situations this can be null.
+                            if (location != null) {
+                               mLocation = location;
+                            }
+                        }
+                    });
+            return;
+        }
     }
 
     //create an observer that watches the LiveData<WeatherData> object
@@ -53,10 +79,13 @@ public class WeatherActivity extends AppCompatActivity {
             if(weatherData!=null) {
                 mTvTempCurrent.setText("" + Math.round(weatherData.getTemperature().getTemp() - 273.15) + " C");
                 mTvHumidity.setText("" + weatherData.getCurrentCondition().getHumidity() + "%");
-                //mTv.setText("" + weatherData.getCurrentCondition().getPressure() + " hPa");
+                mTvWeatherSummary.setText("" + weatherData.getCurrentCondition().getDescr());
             }
         }
     };
 
-
+    void loadWeatherData(String location){
+        //pass the location in to the view model
+        mWeatherViewModel.setLocation(location);
+    }
 }
