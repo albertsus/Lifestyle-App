bd13538f8d0d2ac6b27f30e659a8b24fc139766a
albertsu <suchaofan19950207@gmail.com>
2018-10-23 17:00:33 -0600

    add Login dialog

--- app/src/main/java/cs6018/lifestyleapp/activity/SignupActivity.java
+++ app/src/main/java/cs6018/lifestyleapp/activity/SignupActivity.java
@@ -1,10 +1,13 @@
 package cs6018.lifestyleapp.activity;
 
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -15,7 +18,7 @@ import cs6018.lifestyleapp.utils.Logger;
 public class SignupActivity extends AppCompatActivity {
 
     private EditText mEtUsername, mEtPassword;
-    private String mUsername, mPassword;
+    private String mNewUsername, mNewUserPassword;
     private CardView mCvLogin, mCvSignup;
 
     @Override
@@ -34,22 +37,36 @@ public class SignupActivity extends AppCompatActivity {
         mCvLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                mUsername = mEtUsername.getText().toString();
-                mPassword = mEtPassword.getText().toString();
-                if (UserMatch(mUsername, mPassword)) { // TODO: Check if username:password in db
-                    if (BuildConfig.DEBUG) {
-                        Logger.log("Login Success - username: " + mUsername
-                                + ", password: " + mPassword);
+                AlertDialog.Builder mBuilder = new AlertDialog.Builder(SignupActivity.this);
+                View dialogView = getLayoutInflater().inflate(R.layout.dialog_login, null);
+                final EditText mEmail = (EditText) dialogView.findViewById(R.id.etEmail);
+                final EditText mPassword = (EditText) dialogView.findViewById(R.id.etPassword);
+                Button mLogin = (Button) dialogView.findViewById(R.id.btnLogin);
+                mBuilder.setView(dialogView);
+                final AlertDialog dialog = mBuilder.create();
+                dialog.show();
+                mLogin.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        String mUn = mEmail.getText().toString();
+                        String mPwd = mPassword.getText().toString();
+                        if (UserMatch(mUn, mPwd)) { // TODO: Check if username:password in db
+                            if (BuildConfig.DEBUG) {
+                                Log.v("Login", "Login Success - username: " + mUn
+                                        + ", password: " + mPwd);
+                            }
+                            dialog.dismiss();
+                            startActivity(profileEnterIntent);
+                        } else {
+                            if (BuildConfig.DEBUG) {
+                                Log.v("Login", "Login Fail - username: " + mUn
+                                        + ", password: " + mPwd);
+                            }
+                            Toast.makeText(getApplication(), "Invalid Username/Password, please try again!", Toast.LENGTH_SHORT).show();
+                            //startActivity(profileEnterIntent);
+                        }
                     }
-                    startActivity(profileEnterIntent);
-                } else {
-                    if (BuildConfig.DEBUG) {
-                        Logger.log("Login Fail - username: " + mUsername
-                                + ", password: " + mPassword);
-                    }
-                    // Toast.makeText(getApplication(), "Invalid Username/Password, please try again!", Toast.LENGTH_SHORT).show();
-                    startActivity(profileEnterIntent);
-                }
+                });
             }
         });
 
@@ -57,18 +74,18 @@ public class SignupActivity extends AppCompatActivity {
         mCvSignup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                mUsername = mEtUsername.getText().toString();
-                mPassword = mEtPassword.getText().toString();
-                if (!UserInDB(mUsername)) {
-                    addToDB(mUsername, mPassword); // TODO: add username:password to db
+                mNewUsername = mEtUsername.getText().toString();
+                mNewUserPassword = mEtPassword.getText().toString();
+                if (!UserInDB(mNewUsername)) {
+                    addToDB(mNewUsername, mNewUserPassword); // TODO: add username:password to db
                     if (BuildConfig.DEBUG) {
-                        Logger.log("Signup Success - username: " + mUsername
-                                + ", password: " + mPassword);
+                        Log.v("Signup", "Signup Success - username: " + mNewUsername
+                                + ", password: " + mNewUserPassword);
                     }
                     startActivity(profileEnterIntent);
                 } else {
-                    Logger.log("Signup Fail - username: " + mUsername
-                            + ", password: " + mPassword);
+                    Log.v("Signup", "Signup Fail - username: " + mNewUsername
+                            + ", password: " + mNewUserPassword);
                     Toast.makeText(getApplication(), "Username already existed, please try a new name!", Toast.LENGTH_SHORT).show();
                 }
 
